d8dabb3c867a2461e8e53ac2c8537cf3f59bc7eb
tosh6ko <tosh6ko@gmail.com>
2017-11-14 12:39:49 +0000

    Fixed bugs

--- readibilityscore.js
+++ readibilityscore.js
@@ -4,11 +4,11 @@ function score(input) {
 	var sentence_count =  input.trim().match(/[^\r\n.!?]+(:?(:?\r\n|[\r\n]|[.!?])+|$)/gi).length;
 	var word_count = all_words.length;
 	var difficult_word_count = 0
-	var difficult_word_ratio = difficult_word_count / counts.word
+	var difficult_word_ratio = difficult_word_count / word_count
 	var score= 15.79*difficult_word_ratio + 0.0496*word_count/sentence_count
 	
 	return score
 }
 
 var str = "This is a test. test test test yesss";
-console.log(score(str));
+alert(score(str));
