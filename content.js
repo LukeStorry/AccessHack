26ebebec64d649a514d18da6549aeaceb92f6597
Luke Storry <Luke@LukeStorry.co.uk>
2017-11-14 11:27:15 +0000

    added newtab on butotn press

--- content.js
+++ content.js
@@ -1,2 +1,13 @@
-h1 = "test"
-console.log(h1);
+str = "test"
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "click") {
+            console.log(str);
+            chrome.runtime.sendMessage({
+                "message": "new_tab",
+                "url": str
+            });
+        }
+    }
+);
