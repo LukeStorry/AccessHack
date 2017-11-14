9e6dd003f80ac7231a64dbb06461b738d2b3b515
Luke Storry <Luke@LukeStorry.co.uk>
2017-11-14 13:08:35 +0000

    added html cleaner

--- readibilityscore.js
+++ readibilityscore.js
@@ -1,13 +1,19 @@
 function score(input) {
-	var all_words = input.trim().match(/\b(\w+)\b/g);
-	var sentence_count =  input.trim().match(/[^\r\n.!?]+(:?(:?\r\n|[\r\n]|[.!?])+|$)/gi).length;
+    var span = document.createElement('span');
+    span.innerHTML=input;
+    text = span.textContent || span.innerText;
+
+	var all_words = text.trim().match(/\b(\w+)\b/g);
+	var sentence_count =  text.trim().match(/[^\r\n.!?]+(:?(:?\r\n|[\r\n]|[.!?])+|$)/gi).length;
 	var word_count = all_words.length;
 	var difficult_word_count = 0;
+
     for (word of all_words) {
         if (! common_words.includes(word)){
             difficult_word_count += 1;
         }
     }
+
 	var difficult_word_ratio = difficult_word_count / word_count
 	var score= 15.79*difficult_word_ratio + 0.0496*word_count/sentence_count
 
